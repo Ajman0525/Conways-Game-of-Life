038c93ca51d48fb16a9357b895976787623137f2
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-21 13:57:56 +0800

    feat: add grid lines to the display

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pygame
 
 pygame.init()
 
+# ---- COLOR VARIABLES ---- #
 BLACK = (0,0,0)
 GREY = (128,128,128)
 YELLOW = (255,255,0)
 
-WIDTH, HEIGHT = 600, 600
+# ---- WIDTH AND HEIGHT OF THE SCREEN ---- #
+WIDTH, HEIGHT = 600, 600 
+
+# ---- SIZE OF EVERY SINGLE TILE ---- #
 TILE_SIZE = 20
+
+# ---- NUMBER OF TILES IN THE GRID ---- #
 GRID_WIDTH = WIDTH // TILE_SIZE
 GRID_HEIGHT = HEIGHT // TILE_SIZE
+
+# ---- FRAMES PER SECOND ---- #
 FPS = 60
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -19,8 +27,9 @@ clock = pygame.time.Clock()
 def draw_grid(positions):
     for row in range(GRID_HEIGHT):
         pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
-
-
+    
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(screen, BLACK, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT))
 
 def main():
     running = True
@@ -32,8 +41,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-    pygame.quit
+
+        screen.fill(GREY)   
+        draw_grid(positions)
+        pygame.display.update()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
