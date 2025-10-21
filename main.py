cd7fd9f9523a20f3a71798a292aa44387fe4f4c1
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-21 15:32:52 +0800

    feat: mark and unmark a position when a mouse clicks on a tile
    Marks the position of a tile when a mouse clicks at it and unmark it once a marked tile is clicked again

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 
 pygame.init()
 
+# ---- Window Title ---- #
+pygame.display.set_caption("Conway's Game of Life")
+
 # ---- COLOR VARIABLES ---- #
 BLACK = (0,0,0)
 GREY = (128,128,128)
@@ -25,26 +28,46 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
 def draw_grid(positions):
+
+    for position in positions: # A set containing the positions of the live cells
+        col, row = position
+        top_left = (col * TILE_SIZE, row * TILE_SIZE)
+        pygame.draw.rect(screen, YELLOW, (*top_left, TILE_SIZE, TILE_SIZE)) # Note *top_left will unpack the values so that they would be written individually
+
     for row in range(GRID_HEIGHT):
-        pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
+        pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE)) # Horizontal Grid Lines
     
     for col in range(GRID_WIDTH):
-        pygame.draw.line(screen, BLACK, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT))
+        pygame.draw.line(screen, BLACK, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT)) # Vertical Grid Lines
 
 def main():
     running = True
-    positions = set()
-
+    
+    positions = set() # Contains all the positions of live cells
     while running:
-        clock.tick(FPS)
+        clock.tick(FPS) # Regulate the fps of the loop
 
+        
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # Terminate the loop once the game exits 
                 running = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN: # Mark a position when the mouse is pressed 
+                x, y = pygame.mouse.get_pos()
+                col = x // TILE_SIZE
+                row = y // TILE_SIZE
+                pos = (col, row)
 
-        screen.fill(GREY)   
-        draw_grid(positions)
-        pygame.display.update()
+                if pos in positions: # Note: constant time complexity
+                    positions.remove(pos)
+                else:
+                    positions.add(pos)
+
+
+        screen.fill(GREY)   # Color of the screen
+        draw_grid(positions) # Draw the grids
+        pygame.display.update() # Applies the drawing and show it to the screen
+
 
     pygame.quit()
 
