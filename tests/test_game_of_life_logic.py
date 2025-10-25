8772d539df1b6d8300ecf1bf137e782b59a6c715
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-26 02:13:53 +0800

    feat: add testing functions for the red phase (refs #2)

--- tests/test_game_of_life_logic.py
+++ tests/test_game_of_life_logic.py
@@ -30,3 +30,27 @@ def test_dead_cell_with_two_neighbors_remains_dead():
 #-----------------------#
 #   ISSUE #2 TESTING    #
 #-----------------------#
+
+def test_cell_in_center_has_zero_neighbors():
+    center_test_position = (30, 30)
+    live_neighbors_set = set() # Empty set for zero count
+    assert count_live_neighbors(center_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 0
+
+def test_cell_in_center_has_eight_neighbors():
+    center_test_position = (30, 30)
+    live_neighbors_set = {
+        (29, 29), (30, 29), (31, 29),
+        (29, 30), (31, 30),
+        (29, 31), (30, 31), (31, 31)
+    }
+    assert count_live_neighbors(center_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 8
+
+def test_cell_at_corner_has_max_three_neighbors():
+    top_left_corner_test_position = (0, 0)
+    live_neighbors_set = {(0, 1), (1, 0), (1, 1)}
+    assert count_live_neighbors(top_left_corner_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 3
+
+def test_cell_at_edge_has_max_five_neighbors():
+    top_edge_test_position = (30, 0) 
+    live_neighbors_set = {(29, 0), (31, 0), (29, 1), (30, 1), (31, 1)}
+    assert count_live_neighbors(top_edge_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 5
