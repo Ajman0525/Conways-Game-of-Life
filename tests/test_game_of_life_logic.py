b4ef2cc543d277291c05bd1d6276aa66dc37105f
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-26 16:19:54 +0800

    feat: add tests for issue no. 2 (refs #7)

--- tests/test_game_of_life_logic.py
+++ tests/test_game_of_life_logic.py
@@ -1,8 +1,11 @@
-from src.game_of_life_logic import determine_next_cell_state
-from src.game_of_life_logic import count_live_neighbors
+from src.game_of_life_logic import (
+    determine_next_cell_state, 
+    count_live_neighbors
+)
+from src.main import adjust_grid
 
-# Variables needed for Issue #2 testing
-GRID_WIDTH = 60
+# Variables for counting live neighbors test
+GRID_WIDTH = 60 
 GRID_HEIGHT = 60
 
 #-----------------------#
@@ -29,7 +32,7 @@ def test_dead_cell_with_two_neighbors_remains_dead():
 
 #------------------------------#
 #   DETERMINE LIVE NEIGHBORS   #
-#-----------------------------#
+#------------------------------#
 
 def test_cell_in_center_has_zero_neighbors():
     center_test_position = (30, 30)
@@ -39,14 +42,14 @@ def test_cell_in_center_has_zero_neighbors():
 def test_cell_in_center_has_eight_neighbors():
     center_test_position = (30, 30)
     live_neighbors_set = {
-        (29, 29), # Upper left of the center cell
+        (29, 29), # Upper-left of the center cell
         (30, 29), # Above 
-        (31, 29), # Upper right 
+        (31, 29), # Upper-right 
         (29, 30), # Left side 
         (31, 30), # Right side 
-        (29, 31), # Bottom left
+        (29, 31), # Bottom-left
         (30, 31), # Below 
-        (31, 31)  # Bottom Right
+        (31, 31)  # Bottom-Right
     }
     assert count_live_neighbors(center_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 8
 
@@ -55,7 +58,7 @@ def test_cell_at_corner_has_max_three_neighbors():
     live_neighbors_set = {
         (0, 1), # Below the top corner cell
         (1, 0), # Right 
-        (1, 1)  # Bottom right
+        (1, 1)  # Bottom-right
     }
     assert count_live_neighbors(top_left_corner_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 3
 
@@ -64,8 +67,48 @@ def test_cell_at_edge_has_max_five_neighbors():
     live_neighbors_set = {
         (29, 0), # Left of the top edge cell
         (31, 0), # Right
-        (29, 1), # Bottom left
+        (29, 1), # Bottom-left
         (30, 1), # Below
-        (31, 1)  # Bottom right
+        (31, 1)  # Bottom-right
     }
     assert count_live_neighbors(top_edge_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 5
+
+
+def test_glider_pattern_moves_one_generation():
+    # Glider starts at Top-left:
+    # X . X
+    # . . X
+    # X X X
+    GLIDER_START = {
+        (0, 2), 
+        (1, 0), 
+        (2, 2), 
+        (2, 1), 
+        (2, 0) 
+    }
+
+    # Glider after 1 generation:
+    # . X .
+    # . . X
+    # X X X
+    GLIDER_GENERATION_1 = {
+        (0, 1), 
+        (1, 2), 
+        (2, 2), 
+        (2, 1), 
+        (1, 0)
+    }
+
+    next_generation = adjust_grid(GLIDER_START)
+    assert next_generation == GLIDER_GENERATION_1
+
+def test_block_pattern_is_stable():
+    BLOCK_START = {
+        (10, 10), 
+        (10, 11), 
+        (11, 10), 
+        (11, 11)
+    }
+    expected_generation = BLOCK_START
+    next_generation = adjust_grid(BLOCK_START)
+    assert next_generation == expected_generation
