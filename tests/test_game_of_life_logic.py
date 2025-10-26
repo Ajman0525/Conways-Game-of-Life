a1a994c052842e1ecb3869a4032dc3948c7c31e6
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-26 12:43:40 +0800

    feat: add comments on the grid's coordinates positioning for additional clarity

--- tests/test_game_of_life_logic.py
+++ tests/test_game_of_life_logic.py
@@ -6,7 +6,7 @@ GRID_WIDTH = 60
 GRID_HEIGHT = 60
 
 #-----------------------#
-#   ISSUE #1 TESTING    #
+#    CELL FATE LOGIC    #
 #-----------------------#
 
 def test_survival_with_two_neighbors():
@@ -27,9 +27,9 @@ def test_reproduction_brings_cell_to_life():
 def test_dead_cell_with_two_neighbors_remains_dead():
     assert determine_next_cell_state(is_alive=False, live_neighbors=2) is False
 
-#-----------------------#
-#   ISSUE #2 TESTING    #
-#-----------------------#
+#------------------------------#
+#   DETERMINE LIVE NEIGHBORS   #
+#-----------------------------#
 
 def test_cell_in_center_has_zero_neighbors():
     center_test_position = (30, 30)
@@ -39,18 +39,33 @@ def test_cell_in_center_has_zero_neighbors():
 def test_cell_in_center_has_eight_neighbors():
     center_test_position = (30, 30)
     live_neighbors_set = {
-        (29, 29), (30, 29), (31, 29),
-        (29, 30), (31, 30),
-        (29, 31), (30, 31), (31, 31)
+        (29, 29), # Upper left of the center cell
+        (30, 29), # Above 
+        (31, 29), # Upper right 
+        (29, 30), # Left side 
+        (31, 30), # Right side 
+        (29, 31), # Bottom left
+        (30, 31), # Below 
+        (31, 31)  # Bottom Right
     }
     assert count_live_neighbors(center_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 8
 
 def test_cell_at_corner_has_max_three_neighbors():
     top_left_corner_test_position = (0, 0)
-    live_neighbors_set = {(0, 1), (1, 0), (1, 1)}
+    live_neighbors_set = {
+        (0, 1), # Below the top corner cell
+        (1, 0), # Right 
+        (1, 1)  # Bottom right
+    }
     assert count_live_neighbors(top_left_corner_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 3
 
 def test_cell_at_edge_has_max_five_neighbors():
     top_edge_test_position = (30, 0) 
-    live_neighbors_set = {(29, 0), (31, 0), (29, 1), (30, 1), (31, 1)}
+    live_neighbors_set = {
+        (29, 0), # Left of the top edge cell
+        (31, 0), # Right
+        (29, 1), # Bottom left
+        (30, 1), # Below
+        (31, 1)  # Bottom right
+    }
     assert count_live_neighbors(top_edge_test_position, live_neighbors_set, GRID_WIDTH, GRID_HEIGHT) == 5
